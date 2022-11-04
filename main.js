5e366d7ba8fe230fac0d1a89aa73c6a2be957635
Ebu bekir <112386767+Pioneer444@users.noreply.github.com>
2022-11-04 17:52:32 +0300

    YouTube Dashboard
    my first works

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.text');
+
+readMoreBtn.addEventListener('click',(e)=>{
+    text.classList.toggle('show-more')
+})
